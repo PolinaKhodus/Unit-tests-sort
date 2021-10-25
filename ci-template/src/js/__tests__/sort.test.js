e413ab465d8b6c0c48f0c02c1bba19ab3c8329d9
Polina <sgsey75@gmail.com>
2021-10-26 00:19:32 +0300

    solution Unit-tests sort

---
+++ ci-template/src/js/__tests__/sort.test.js
@@ -0,0 +1,18 @@
+import SortPerson from '../../sort';
+
+
+test('should health', () => {
+  const result = SortPerson([
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ]
+  );
+
+  expect(result).toEqual([
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ]
+  );
+});
---
+++ ci-template/src/sort.js
@@ -0,0 +1,14 @@
+export default function SortPerson(persons) {
+    persons.sort(function (a, b) {
+      if (a.health > b.health) {
+        return -1;
+      }
+      if (a.health < b.health) {
+        return 1;
+      }
+      
+      return 0;
+    });
+    return persons;
+  }
+  
